1d3d2a81046f024777777c1bc45c73acde42234c
mishgan55 <vorota.mogilev@gmail.com>
2023-07-30 12:37:21 +0300

    Created Repositories for Book and Person will use SringData JPA

---
+++ src/main/java/khorsun/hibernate/library/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package khorsun.hibernate.library.repositories;
+
+import khorsun.hibernate.library.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Integer> {
+}
---
+++ src/main/java/khorsun/hibernate/library/services/PersonService.java
@@ -0,0 +1,21 @@
+package khorsun.hibernate.library.services;
+
+import khorsun.hibernate.library.models.Person;
+import khorsun.hibernate.library.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonService {
+    private final PersonRepository personRepository;
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public List<Person> findAll(){
+        return personRepository.findAll();
+    }
+}
